f72bb27b0bc1dd277e13e3e8e47a15d318cf466c
syun39 <iinosyun395@gmail.com>
2025-07-13 17:42:18 +0900

    コメント追加

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import Practice from './Practice.jsx'
 
+// コードを修正したあと npm run deployとターミナルに打ち込む
+
 createRoot(document.getElementById('root')).render(
     <StrictMode>
        <Practice/>
